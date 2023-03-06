551787361d500e20f225d1fe26d8535aee661d40
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-06 17:33:01 +0300

    links to socials

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import Action from './action'
 import MyImage from '../../images/me.jpeg'
+import Socials from './socials'
 
 
 function Header() {
@@ -23,6 +24,7 @@ function Header() {
           <h5 className="text-light">I am a FullStack Developer.</h5>
 
           <Action/>
+          <Socials/>
 
           <div className="me">
 
--- src/components/header/socials.jsx
+++ src/components/header/socials.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import {BsLinkedin, BsGithub, BsYoutube, BsMedium, BsTwitter} from 'react-icons/bs'
+import {SiLinktree} from 'react-icons/si'
 
 
 function Socials() {
@@ -13,9 +14,45 @@ function Socials() {
     <div className='header-socials'>
         
         
+        <a href='https://github.com/Wanjie-Ryan' target ='_blank' rel="noreferrer">
+
+            <BsGithub/>
+
+        </a>
+
+
+
+        <a href='https://www.youtube.com/channel/UC1Fu4FFqsOR-pGxkQVQTTsg' target ='_blank' rel="noreferrer">
+
+            <BsYoutube/>
+        </a>
+
+
+        <a href='https://medium.com/@ryanwanjiedistortion' target ='_blank' rel="noreferrer">
+
+            <BsMedium/>
+
+        </a>
+
+
+        <a href='https://www.linkedin.com/in/ryan-wanjie-7797831b5/' target ='_blank' rel="noreferrer">
+
+            <BsLinkedin/>
+        </a>
+
+        <a href ='https://twitter.com/RWanjie' target ='_blank' rel="noreferrer"> 
         
-        
-        
+             <BsTwitter/>
+        </a>
+
+
+        <a href ='https://linktr.ee/wanjieryan' target ='_blank' rel="noreferrer">
+
+            <SiLinktree/>
+        </a>
+
+
+
     </div>
 
     </>
