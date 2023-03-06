cdfb73e998dcaaf47a6a132a25b2de9892053cce
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-06 16:11:08 +0300

    header

---
+++ src/components/header/action.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Action() {
+
+
+  return (
+
+    
+    <div>Action</div>
+  )
+}
+
+export default Action
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import './header.css'
 
 
+
+
 function Header() {
+
+
+
+
   return (
-    <div>Header</div>
+
+    <>
+
+      <header>
+
+        <div className="container header_container">
+
+          <h5>Hello,&#128075; I Go By the Name,</h5>
+          <h1>Wanjie Ryan.</h1> 
+
+          <h5 className="text-light">I am a FullStack Developer.</h5>
+
+        </div>
+
+
+
+
+
+      </header>
+    
+    
+    
+    
+    </>
+
+
   )
 }
 
