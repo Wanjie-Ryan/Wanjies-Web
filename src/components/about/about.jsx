27b0e3dfff39469e13493f33a9cc37c221b81dc0
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-07 19:28:30 +0300

    about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -10,7 +10,40 @@ function About() {
 
     <>
     
-    <section id ='about'>About</section>
+    <section id ='about'>
+      
+      <h5>Get To Know</h5>
+      <h2>About Wanjie Ryan</h2>
+      
+      
+      <div className="container about-container">
+
+        <div className="about-me">
+          <div className="about-me-img">
+
+              <img src ='' alt = ''/>
+
+          </div>
+
+
+        </div>
+
+
+        <div className="about-content">
+
+
+          
+        </div>
+
+
+      </div>
+
+
+
+
+      
+      
+    </section>
     
     
     </>
