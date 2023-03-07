19820f4670130a94eaa6a63514b902c9f39a3017
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-07 15:23:03 +0300

    nav component

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './nav.css'
+import {BiHomeSmile} from 'react-icons/bi'
 
 
 function Nav() {
+
+
+
   return (
-    <div>Nav</div>
+
+    <>
+
+
+      <nav>
+
+
+        <a href ='#'> <BiHomeSmile/> </a>
+        <a href ='#about'> <BiHomeSmile/> </a>
+        <a href ='#experience'> <BiHomeSmile/> </a>
+        <a href ='#services'> <BiHomeSmile/> </a>
+        <a href ='#contact'> <BiHomeSmile/> </a>
+
+
+
+
+
+
+
+
+      </nav>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+
+
+
   )
 }
 
