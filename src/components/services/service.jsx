2d612e2b1eaa87bc2380206ea7df9cfac3414162
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-08 22:39:42 +0300

    service page

--- src/components/services/service.jsx
+++ src/components/services/service.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './service.css'
-
+import {BiCheck} from 'react-icons/bi'
 
 
 function Service() {
@@ -11,7 +11,51 @@ function Service() {
     <>
     
     
-    <section id ='services' >Service</section>
+    <section id ='services' >
+
+      <h5>What I Offer...</h5>
+      <h2>Services</h2>
+
+
+      <div className="container services-container">
+
+          <article className="service">
+
+            <div className="service-head">
+
+              <h3>Machine Learning & Data Visualization.</h3>
+
+            </div>
+
+
+            <ul className="service-list">
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Creating Models using Python.</p>
+              </li>
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Performing data Visualization and analysis.</p>
+              </li>
+
+
+
+            </ul>
+
+          </article>
+
+
+      </div>
+
+
+
+    </section>
     
     </>
 
