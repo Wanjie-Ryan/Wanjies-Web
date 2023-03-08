e3bf8de7a220f9e549755d2d144cb5b26edecd72
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-08 22:48:29 +0300

    services page

--- src/components/services/service.jsx
+++ src/components/services/service.jsx
@@ -44,11 +44,119 @@ function Service() {
                 <p>Performing data Visualization and analysis.</p>
               </li>
 
+            </ul>
+
+          </article>
+
+
+
+
+          <article className="service">
+
+            <div className="service-head">
+
+              <h3>Web Development.</h3>
+
+            </div>
+
+
+            <ul className="service-list">
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Creating nice-looking and fully functional websites.</p>
+              </li>
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Creating and Managing Databases.</p>
+              </li>
+
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Optimizing Your website and make it better.</p>
+              </li>
+
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>UI/UX designing using Figma.</p>
+              </li>
+
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Hosting your Website.</p>
+              </li>
+
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Integrating APIs such as Payment services.</p>
+              </li>
+
+            </ul>
+
+          </article>
+
+
+
+
+          <article className="service">
+
+            <div className="service-head">
+
+              <h3>Content Creation.</h3>
+
+            </div>
+
+
+            <ul className="service-list">
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Article and Blog writing.</p>
+              </li>
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Academic writing.</p>
+              </li>
+
+
+              <li>
+
+                <BiCheck className = 'icon'/>
+
+                <p>Solving computational problems.</p>
+              </li>
+
 
 
             </ul>
 
           </article>
+
+
+
+
 
 
       </div>
