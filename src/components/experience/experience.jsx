904d7b793d0d4957ce290c018f6be15ffaf54f54
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-08 07:22:12 +0300

    experience component

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './experience.css'
-import {FaHtml5,FaReact,FaPython,FaJava} from 'react-icons/fa'
+import {FaHtml5,FaReact,FaPython,FaJava,FaNodeJs} from 'react-icons/fa'
 import {RiCss3Fill} from 'react-icons/ri'
 import {TbBrandJavascript} from 'react-icons/tb'
 import {BsGithub} from 'react-icons/bs'
+import {SiXampp,SiPostgresql,SiPostman,SiMongodb} from 'react-icons/si'
+
+
 
 function Experience() {
 
@@ -100,45 +103,28 @@ function Experience() {
         </div>
 
 
+
+
         <div className="exp-backend">
 
-            <h3>Backend Tools</h3>
+            <h3>Backend Development Tools</h3>
 
           <div className="exp-content">
 
                   <article className="exp-details">
 
-                    <FaHtml5/>
-                    <h4>HTML</h4>
-                    <small className="text-light">I have a diverse experience in <b>HTML</b>.</small>
+                    <SiXampp/>
+                    <h4>XAMPP</h4>
+                    <small className="text-light">I have a diverse experience in SQL <b>XAMPP</b>.</small>
 
                   </article>
 
 
                   <article className="exp-details">
 
-                    <RiCss3Fill/>
-                    <h4>CSS</h4>
-                    <small className="text-light">I have a diverse experience in <b>CSS</b>.</small>
-
-                  </article>
-
-
-
-                  <article className="exp-details">
-
-                    <TbBrandJavascript/>
-                    <h4>Javascript</h4>
-                    <small className="text-light">I have a diverse experience in <b>JavaScript</b>.</small>
-
-                  </article>
-
-
-                  <article className="exp-details">
-
-                    <FaReact/>
-                    <h4>ReactJs</h4>
-                    <small className="text-light">I have a diverse experience in <b>ReactJS</b>.</small>
+                    <SiPostgresql/>
+                    <h4>PostgreSQL</h4>
+                    <small className="text-light">I have a diverse experience in SQL <b>PostgreSQL</b>.</small>
 
                   </article>
 
@@ -146,32 +132,33 @@ function Experience() {
 
                   <article className="exp-details">
 
-                    <FaPython/>
-                    <h4>Python</h4>
-                    <small className="text-light">I have a diverse experience in <b>Python(Machine Learning and Data Visualization.)</b>.</small>
+                    <SiPostman/>
+                    <h4>Postman</h4>
+                    <small className="text-light">I have a diverse experience in using <b>Postman</b>.</small>
 
                   </article>
 
 
-                  <article className='exp-details'>
+                  <article className="exp-details">
 
-                    <FaJava/>
-                    <h4>Java</h4>
-                    <small className="text-light">I have a diverse experience in <b>Java</b>.</small>
+                    <FaNodeJs/>
+                    <h4>Node and ExpressJS</h4>
+                    <small className="text-light">I have a diverse experience in <b>Node and ExpressJS</b>.</small>
 
                   </article>
 
 
 
-                  <article className='exp-details'>
+                  <article className="exp-details">
 
-                    <BsGithub/>
-                    <h4>Git and Github.</h4>
-                    <small className="text-light">I have a diverse experience in <b>Github and Git commands</b>.</small>
+                    <SiMongodb/>
+                    <h4>MongoDB</h4>
+                    <small className="text-light">I have a diverse experience in NON-SQL <b>MongoDB</b>.</small>
 
-                  </article>    
+                  </article>
 
 
+                  
           </div>
 
         </div>
