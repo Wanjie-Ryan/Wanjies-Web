62f4ede5fdcc01840a2da894a428d25e5f93ead1
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-09 07:55:17 +0300

    portfolio

--- src/components/portfolio/port.jsx
+++ src/components/portfolio/port.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './port.css'
-
+import video from '../../images/waka.mp4'
 
 function Port() {
 
@@ -22,10 +22,16 @@ function Port() {
 
             <div className="portimage">
 
-
-
-              
+            <video controls>
+              <source src={video} type="video/mp4" />
+              Your browser does not support the video.
+            </video>
             </div>
+
+
+            <h3>Wakawaka Real Estate Management System.</h3>
+            <a href ='https://github.com/orgs/Astro-Devs-Tech/repositories' className='btn' target = '_blank' rel="noreferrer">Github</a>
+            <a href ='https://www.youtube.com/watch?v=_I9UakCEOnY&t=1s&ab_channel=RyanWanjie' className='btn btn-primary' target = '_blank' rel="noreferrer">Live Demo</a>
 
 
 
