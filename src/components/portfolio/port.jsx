6b098860b7f5bdc8ef00930e4cbfc23f6f94a1dd
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-09 08:05:24 +0300

    portfolio

--- src/components/portfolio/port.jsx
+++ src/components/portfolio/port.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './port.css'
 import video from '../../images/waka.mp4'
+import hive from '../../images/beehive.png'
+
+
 
 function Port() {
 
@@ -35,8 +38,46 @@ function Port() {
 
 
 
+          </article>
+
+
+          <article className="portfolio-item">
+
+            <div className="portimage">
+
+              <img src = {hive} alt =''/>
+            </div>
+
+
+            <h3>Superior Beehive.</h3>
+            <a href ='https://github.com/Wanjie-Ryan/BeeTree-client' className='btn' target = '_blank' rel="noreferrer">Github</a>
+            <a href ='https://thybee.netlify.app/' className='btn btn-primary' target = '_blank' rel="noreferrer">Live Demo</a>
+
+
 
           </article>
+
+
+
+          <article className="portfolio-item">
+
+            <div className="portimage">
+
+            <img src = '' alt = ''/>
+            </div>
+
+
+            <h3>Rawa Booking App.</h3>
+            <a href ='https://github.com/Wanjie-Ryan/Modern-Reservations-App' className='btn' target = '_blank' rel="noreferrer">Github</a>
+            <a href ='https://www.youtube.com/watch?v=_I9UakCEOnY&t=1s&ab_channel=RyanWanjie' className='btn btn-primary' target = '_blank' rel="noreferrer">Live Demo</a>
+
+
+
+          </article>
+
+
+
+
 
 
 
