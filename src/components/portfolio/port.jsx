0d60620d12355cf75006f68efdbf226a903652e1
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-09 07:31:10 +0300

    portfolio

--- src/components/portfolio/port.jsx
+++ src/components/portfolio/port.jsx
@@ -3,8 +3,60 @@ import './port.css'
 
 
 function Port() {
+
+
+
   return (
-    <div>Port</div>
+
+    <>
+    
+      <section id="portfolio">
+
+
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+
+        <div className="container portfolio-container">
+
+          <article className="portfolio-item">
+
+            <div className="portimage">
+
+
+
+              
+            </div>
+
+
+
+
+          </article>
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+      </section>
+    
+    
+
+
+
+    
+    </>
+
+
+
+
+
   )
 }
 
