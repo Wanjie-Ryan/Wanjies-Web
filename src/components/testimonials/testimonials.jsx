b8b9361ee9a1f4e4e5c7836e8df1fb935638eccf
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-09 19:01:21 +0300

    testimonials

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -2,9 +2,60 @@ import React from 'react'
 import './testimonials.css'
 
 
+
+
 function Testimonials() {
+
+
+
   return (
-    <div>Testimonials</div>
+
+
+      <section id="testimonials">
+
+        <h5>Reviews from Clients.</h5>
+        <h2>Testimonials</h2>
+
+        <div className="container tes-container">
+
+          <article className="test">
+
+
+            <div className="client-details">
+
+              <h5 className="client-name">Philip Waruinge</h5>
+              <small className="client-rev">Developed the Superior Beehive website for me, I highly Recommend.</small>
+
+
+            </div>
+
+
+
+
+          </article>
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+      </section>
+
+
+
+
   )
 }
 
