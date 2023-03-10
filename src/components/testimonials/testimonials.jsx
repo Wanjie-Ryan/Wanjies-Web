da1cefb39fd4d0f33db1d4be2a06260c7ac61d20
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-10 08:37:36 +0300

    mapping data

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -6,6 +6,34 @@ import './testimonials.css'
 
 function Testimonials() {
 
+  const data =[
+
+    {
+      id:1,
+      name:'Astro Devs Group',
+      review:'We collaborated with Wanjie in building the Hackathon project; Real Estate management system, he is highly skilled lad and great to work with.'
+    },
+
+    {
+      id:2,
+      name:'Philip Waruinge',
+      review:'Developed the Superior Beehive website for me, I highly Recommend.'
+
+    },
+
+    {
+
+      id:3,
+      name:'Wanjie Ryan',
+      review:'Developed the Rawa Booking App, it is a great site that can boost the customers coming to your hotel, I highly recommend Wanjie.'
+
+    }
+
+
+
+
+  ]
+
 
 
   return (
@@ -16,30 +44,38 @@ function Testimonials() {
         <h5>Reviews from Clients.</h5>
         <h2 className='bounce'>Testimonials</h2>
 
+
+
         <div className="container tes-container">
 
-          <article className="test">
+          {
+
+            data.map(({id, name, review})=>{
+
+              return(
+
+                <article className="test">
 
 
-            <div className="client-details">
+                  <div className="client-details">
 
-              <h5 className="client-name">Philip Waruinge</h5>
-              <small className="client-rev">Developed the Superior Beehive website for me, I highly Recommend.</small>
-
-
-            </div>
+                    <h5 className="client-name"></h5>
+                    <small className="client-rev"></small>
 
 
-
-
-          </article>
-
-
-
+                  </div>
 
 
 
 
+                </article>
+              )
+
+
+            })
+
+
+          }
 
         </div>
 
