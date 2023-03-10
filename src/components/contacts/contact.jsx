566351906f3c9d3f6bfe8231c7e1360f572afdc7
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-10 09:41:36 +0300

    contact

--- src/components/contacts/contact.jsx
+++ src/components/contacts/contact.jsx
@@ -27,7 +27,7 @@ function Contact() {
             <MdEmail/>
             <h4>Email</h4>
             {/* <h5>ryanwanjie1@gmail.com</h5> */}
-            <a href ='mailto:ryanwanjie1@gmail.com'>Send an Email</a>
+            <a href ='mailto:ryanwanjie1@gmail.com' target ='_blank' rel="noreferrer">Send an Email</a>
 
 
 
@@ -39,7 +39,7 @@ function Contact() {
             <BiPhoneCall/>
             <h4>Call me</h4>
             {/* <h5>ryanwanjie1@gmail.com</h5> */}
-            <a href ='tel:+254791618714'>Call me, Let's engage.</a>
+            <a href ='tel:+254791618714' target ='_blank' rel="noreferrer">Call me, Let's engage.</a>
 
 
 
@@ -51,7 +51,7 @@ function Contact() {
             <BsWhatsapp/>
             <h4>Whatsapp</h4>
             {/* <h5>ryanwanjie1@gmail.com</h5> */}
-            <a href="https://api.whatsapp.com/send?phone=254791618714">Reach me on Whatsapp</a>
+            <a href="https://api.whatsapp.com/send?phone=254791618714" target ='_blank' rel="noreferrer" >Reach me on Whatsapp</a>
 
 
 
