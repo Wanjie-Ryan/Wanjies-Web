999b2c31f2b3a5f9a1213b01d807640b9832ccd9
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-10 09:28:26 +0300

    contact

--- src/components/contacts/contact.jsx
+++ src/components/contacts/contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './contact.css'
-
+import {MdEmail} from 'react-icons/md'
 
 
 
@@ -14,7 +14,67 @@ function Contact() {
     <section id ='contact'>
 
       <h5>Get In Touch</h5>
-      <h2 class ='bounce'>Contact Me.</h2>
+      <h2 class ='bounce'>Contact Me</h2>
+
+
+      <div className="container contact-container">
+
+        <div className="contact-options">
+
+
+          <article className="contact-option">
+
+            <MdEmail/>
+            <h4>Email</h4>
+            <h5>ryanwanjie1@gmail.com</h5>
+            <a href ='mailto:ryanwanjie1@gmail.com'>Send an Email</a>
+
+
+
+          </article>
+
+
+          {/* <article className="contact-option">
+
+            <MdEmail/>
+            <h4>Email</h4>
+            <h5>ryanwanjie1@gmail.com</h5>
+            <a href ='mailto:ryanwanjie1@gmail.com'>Send an Email</a>
+
+
+
+          </article> */}
+
+
+          <article className="contact-option">
+
+            <MdEmail/>
+            <h4>Email</h4>
+            <h5>ryanwanjie1@gmail.com</h5>
+            <a href ='mailto:ryanwanjie1@gmail.com'>Send an Email</a>
+
+
+
+          </article>
+
+
+
+
+
+
+
+
+        </div>
+
+        <form action =''>
+
+
+
+
+
+        </form>
+
+      </div>
 
 
 
