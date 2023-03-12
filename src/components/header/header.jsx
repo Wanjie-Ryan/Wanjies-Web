18e182ad216693faa1cebfc59546ce707b8c7f92
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-12 18:28:37 +0300

    theme using context

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,11 +3,19 @@ import './header.css'
 import Action from './action'
 import MyImage from '../../images/me.jpeg'
 import Socials from './socials'
+import {BsToggleOn} from 'react-icons/bs'
 
 
 function Header() {
 
+    // const themestyles = {
 
+
+    //   backgroundColor: darktheme ? '#1f1f38'  : '#ccc',
+    //   color:darktheme ? '#fff' : '#1f1f38'
+
+
+    // }
 
 
   return (
@@ -17,6 +25,8 @@ function Header() {
       <header id = 'home'>
 
         <div className="container header_container">
+
+          <BsToggleOn className='toggle'/>
 
           <h5>Hello,&#128075; I Go By the Name,</h5>
           <h1 className='slide'>Wanjie Ryan.</h1> 
---
+++ src/context.jsx
@@ -0,0 +1,24 @@
+import React, {useContext, useState} from 'react'
+
+
+
+const Themecontext = React.createContext()
+
+const Themeupdatecontext = React.createContext()
+
+
+
+export function Themeprovider() {
+
+
+
+  return (
+
+
+    <div>context</div>
+
+
+
+  )
+}
+
