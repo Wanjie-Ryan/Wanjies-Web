4ddc87fa22e7db3365730f7dad4b966b9de1bdc3
Ryan <ryanwanjiedistortion@gmail.com>
2023-03-13 19:42:02 +0300

    final

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,35 +3,38 @@ import './header.css'
 import Action from './action'
 import MyImage from '../../images/me.jpeg'
 import Socials from './socials'
-import {BsToggleOn} from 'react-icons/bs'
-import { UseTheme, Usethemeupdate } from '../../context'
+// import {BsToggleOn} from 'react-icons/bs'
+// import { UseTheme, Usethemeupdate } from '../../context'
 
 function Header() {
 
 
-    const darktheme = UseTheme()
-    const toggletheme = Usethemeupdate()
+    // const darktheme = UseTheme()
+    // const toggletheme = Usethemeupdate()
 
-    const themestyles = {
+    // const themestyles = {
 
 
-      backgroundImage: darktheme ? `url(${require('../../../src/images/darkbluetexture.jpg')})` : `url(${require('../../../src/images/bluetexture (1).jpg')})`,
+    //   backgroundImage: darktheme ? `url(${require('../../../src/images/darkbluetexture.jpg')})` : `url(${require('../../../src/images/bluetexture (1).jpg')})`,
   
-      color:darktheme ? '#fff' : '#1f1f38'
+    //   color:darktheme ? '#fff' : '#1f1f38'
 
 
-    }
+    // }
 
+    // style ={themestyles}
+
+    // onClick={toggletheme}
 
   return (
 
     <>
 
-      <header id = 'home' styles ={themestyles}>
+      <header id = 'home' >
 
-        <div className="container header_container">
+        <div className="container header_container" >
 
-          <BsToggleOn className='toggle' onClick={toggletheme}/>
+          {/* <BsToggleOn className='toggle' /> */}
 
           <h5>Hello,&#128075; I Go By the Name,</h5>
           <h1 className='slide'>Wanjie Ryan.</h1> 
