14c3f5d9c9b2516db5f64ccb5d772245c5584922
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-04-23 17:57:40 +0300

    personal portfolio

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,110 +1,69 @@
-import React from 'react'
-import './about.css'
-import MyImage from '../../images/me.jpeg'
-import {FaAward,FaUsers} from 'react-icons/fa'
-import {BsFolderFill} from 'react-icons/bs'
-
-
+import React from "react";
+import "./about.css";
+import MyImage from "../../images/me.jpeg";
+import { FaAward, FaUsers } from "react-icons/fa";
+import { BsFolderFill } from "react-icons/bs";
+import Pic1 from "../../images/pic1.jpg";
 
 function About() {
-
-
   return (
-
     <>
-    
-    <section id ='about'>
-      
-      <h5>Get To Know</h5>
-      <h2 className='bounce'>About Wanjie Ryan</h2>
-      
-      
-      <div className="container about-container">
+      <section id="about">
+        <h5>Get To Know</h5>
+        <h2 className="bounce">About Wanjie Ryan</h2>
 
-        <div className="about-me">
-          <div className="about-me-img">
-
-              <img src ={MyImage} alt = ''/>
-
+        <div className="container about-container">
+          <div className="about-me">
+            <div className="about-me-img">
+              <img src={Pic1} alt="" />
+            </div>
           </div>
 
+          <div className="about-content">
+            <div className="about-cards">
+              <article className="about-card">
+                <FaAward className="about-icon" />
+                <h5>Experience</h5>
+                <small>3+ Years Working Experience.</small>
+              </article>
 
-        </div>
+              <article className="about-card">
+                <FaUsers className="about-icon" />
+                <h5>Clients</h5>
+                <small>
+                  Worked with over 3 clients countrywide.
+                  <br></br>
+                  You've Got a project? Contact me.
+                </small>
+              </article>
 
-
-        <div className="about-content">
-          <div className="about-cards">
-
-            <article className='about-card'>
-
-              <FaAward className='about-icon'/>
-              <h5>Experience</h5>
-              <small>3+ Years Working Experience.</small>
-
-
-            </article>
-
-            <article className='about-card'>
-
-              <FaUsers className='about-icon'/>
-              <h5>Clients</h5>
-              <small>
-                Worked with over 3 clients countrywide.
-                <br></br>
-                You've Got a project? Contact me.</small>
-
-            </article>
-
-
-            <article className='about-card'>
-
-              <BsFolderFill className='about-icon'/>
-              <h5>Projects</h5>
-              <small>Completed over 40+ both Mini and Major Projects.</small>
-
-
-            </article>
-          </div>
-
+              <article className="about-card">
+                <BsFolderFill className="about-icon" />
+                <h5>Projects</h5>
+                <small>Completed over 40+ both Mini and Major Projects.</small>
+              </article>
+            </div>
 
             <p>
-
-              I am an Experienced Software Developer, I love Programming and I am always learning and seeking to know new Tricks and Knowledge.
-
+              I am an Experienced Software Developer, I love Programming and I
+              am always learning and seeking to know new Tricks and Knowledge.
               <br></br>
-              Apart from Programming, I also love football, not only an Fc Barcelona fan,but also playing.  
-
+              Apart from Programming, I also love football, not only an Fc
+              Barcelona fan,but also playing.
+              <br></br>I also Love Hiking, have you gone to Lukenya Hills? Such
+              an awesome place. I also play chess, bet you cannot beat me there.
               <br></br>
-
-              I also Love Hiking, have you gone to Lukenya Hills? Such an awesome place. I also play chess, bet you cannot beat me there.
-
-              <br></br>
-
               <b>When It Rains, It Pours.</b>
-
-
             </p>
 
-
-
-            <a href ='#contact' className='btn btn-primary'>Let's Talk</a>
-
-
+            <a href="#contact" className="btn btn-primary">
+              Let's Talk
+            </a>
+          </div>
         </div>
-
-
-      </div>
-
-
-
-    </section>
-    
-    
+      </section>
     </>
-
-
-
-  )
+  );
 }
 
-export default About
+export default About;
